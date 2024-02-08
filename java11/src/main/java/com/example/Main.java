075a355d671e537f9bd361ee8485dc6c1886b707
su-guru <su-guru@example.com>
2024-02-08 23:17:18 +0900

    add samples of the collection api on java 8, 11.

--- java11/src/main/java/com/example/Main.java
+++ java11/src/main/java/com/example/Main.java
@@ -1,7 +1,9 @@
 package com.example;
 
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Stream.of("c", "b", "a").sorted().forEach(System.out::println);
     }
 }
--- java8/src/main/java/com/example/Main.java
+++ java8/src/main/java/com/example/Main.java
@@ -1,7 +1,14 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<String> objects = new ArrayList<>();
+        objects.add("c");
+        objects.add("b");
+        objects.add("a");
+        objects.stream().sorted().forEach(System.out::println);
     }
 }
