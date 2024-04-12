e1fa1aed6d2d084180a93dbf15f55c3aba23f6f3
su-guru <43238724+su-guru@users.noreply.github.com>
2024-04-12 23:20:30 +0900

    IntStream iterate 追加

--- java11/src/main/java/com/example/Main.java
+++ java11/src/main/java/com/example/Main.java
@@ -1,7 +1,13 @@
 package com.example;
 
+import java.util.stream.IntStream;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // java 9 IntStream.iterate
+        // rangeClosedより見やすい
+        // [0]に初期値 [1]に後続の関数をつづける条件 [2]に条件まで続ける関数を渡す
+        IntStream.iterate(1, year -> year < 10, year -> year + 1)
+                .forEach(System.out::println);
     }
 }
